333e57cfe6ff3f64d74a75a6ea0c9482ca905cbb
Abdul Ghaffar <aghaffar570@gmail.com>
2018-07-25 13:04:51 -0400

    add random circles

---
+++ index.js
@@ -0,0 +1,15 @@
+const paper = require('paper');
+
+paper.install(window);
+window.onload = function() {
+	paper.setup('myCanvas');
+	var path = new Path.Rectangle([
+		75, 75,
+	], [100, 100,]);
+	path.strokeColor = 'black';
+
+	view.onFrame = function(event) {
+		On each frame,
+		rotate the path by 3 degrees : path.rotate(3);
+	}
+}
